34059e131d9fed4bec65a44468851b91f9234fc9
leandro992 <victorleandro100@outlook.com>
2022-06-28 19:49:51 -0300

    create project

---
+++ src/main/java/com/alura/Application.java
@@ -0,0 +1,9 @@
+package com.alura;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
