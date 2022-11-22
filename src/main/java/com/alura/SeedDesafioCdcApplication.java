fb91f963c7ba257687fcc0a9fa12c4e8dc9c3825
victor.leandro <victorleandro100@outlook.com>
2022-11-21 21:59:18 -0300

    rename project

--- src/main/java/com/alura/SeedDesafioCdcApplication.java
+++ src/main/java/com/alura/SeedDesafioCdcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SeedDesafioCdc01Application {
+public class SeedDesafioCdcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SeedDesafioCdc01Application.class, args);
+		SpringApplication.run(SeedDesafioCdcApplication.class, args);
 	}
 
 }
--- src/test/java/com/alura/SeedDesafioCdcApplicationTests.java
+++ src/test/java/com/alura/SeedDesafioCdcApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SeedDesafioCdc01ApplicationTests {
+class SeedDesafioCdcApplicationTests {
 
 	@Test
 	void contextLoads() {
